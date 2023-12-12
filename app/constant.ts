af60a5ecacf1a551f8437ad6f0adb55f8a1f1178
keney2023 <134574829+keney2023@users.noreply.github.com>
2023-12-12 15:55:57 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
-export const OWNER = "Hk-Gosuto";
-export const REPO = "ChatGPT-Next-Web-LangChain";
+export const OWNER = "KeNey";
+export const REPO = "KeNey On ChatGpt";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
